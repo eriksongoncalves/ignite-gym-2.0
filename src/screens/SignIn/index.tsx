0b717068d0be4fc60a466bb5f845eda9d304fc3c
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2024-09-08 17:58:54 -0300

    feat: tailwind theme

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 export function SignIn() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-2xl font-bold">SignIn</Text>
+      <Text className="text-white text-2xl font-robotoBold">SignIn</Text>
     </View>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,35 @@
 module.exports = {
   content: ['./App.tsx', './src/**/*.{ts,tsx}'],
   theme: {
+    fontFamily: {
+      robotoRegular: ['Roboto_400Regular'],
+      robotoBold: ['Roboto_700Bold']
+    },
+    fontSize: {
+      'text-base': '14px',
+      xs: '12px',
+      sm: '14px',
+      lg: '16px',
+      xl: '18px',
+      '2xl': '24px'
+    },
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      gray: {
+        100: '#e1e1e6',
+        200: '#c4c4cc',
+        300: '#7c7c8a',
+        400: '#323238',
+        500: '#29292e',
+        600: '#202024',
+        700: '#121214'
+      },
+      green: {
+        500: '#00b37e',
+        700: '#00875f'
+      }
+    },
     extend: {}
   },
   plugins: []
