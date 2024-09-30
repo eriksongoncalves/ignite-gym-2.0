218bbce0744e8de22765b97511a1c51e6120b011
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2024-09-30 13:48:47 -0300

    feat: add deep linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,29 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { LinkingOptions, NavigationContainer } from '@react-navigation/native'
 import { useAuth } from '@hooks/auth'
 
 import { AppTabRoutes } from './app.tab.routes'
 import { AuthStackRoutes } from './auth.stack.routes'
 
+const linking: LinkingOptions<ReactNavigation.RootParamList> = {
+  prefixes: ['ignitegym://', 'com.teste.ignitegym://'],
+  config: {
+    screens: {
+      exercise: {
+        path: 'exercise/:exerciseId',
+        exact: true,
+        parse: {
+          exerciseId: (exerciseId: string) => exerciseId
+        }
+      }
+    }
+  }
+}
+
 export function Routes() {
   const { user } = useAuth()
 
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       {user?.id ? <AppTabRoutes /> : <AuthStackRoutes />}
     </NavigationContainer>
   )
